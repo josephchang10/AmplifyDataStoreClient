1cfdc224d17bdcde04dbd5a4acc5045cf73af410
Jiafu Zhang <zhangkuan523@gmail.com>
2023-12-23 17:49:02 +0800

    Add AmplifyDataStoreClient dependencies

--- Package.swift
+++ Package.swift
@@ -5,17 +5,32 @@ import PackageDescription
 
 let package = Package(
     name: "AmplifyDataStoreClient",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AmplifyDataStoreClient",
             targets: ["AmplifyDataStoreClient"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
+        .package(url: "https://github.com/replicate/replicate-swift", from: "0.20.0"),
+        .package(url: "https://github.com/aws-amplify/amplify-swift", from: "2.25.1")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AmplifyDataStoreClient"),
+            name: "AmplifyDataStoreClient",
+            dependencies: [
+                .product(name: "Replicate", package: "replicate-swift"),
+                .product(name: "Amplify", package: "amplify-swift"),
+                .product(name: "Dependencies", package: "swift-dependencies")
+            ]
+        ),
         .testTarget(
             name: "AmplifyDataStoreClientTests",
             dependencies: ["AmplifyDataStoreClient"]),
--- Sources/AmplifyDataStoreClient/AmplifyDataStoreClient.swift
+++ Sources/AmplifyDataStoreClient/AmplifyDataStoreClient.swift
@@ -1,2 +1,59 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Combine
+import Amplify
+import Dependencies
+
+struct DataStoreClient {
+    var getReady: () async -> Void
+}
+
+extension DataStoreClient: DependencyKey {
+    static var liveValue = {
+        let manager = DataStoreManager()
+        return Self(
+            getReady: { await manager.getReady() }
+        )
+    }()
+}
+
+private class DataStoreManager {
+    @Published var isReady = false
+    private var hubListener: AnyCancellable?
+    
+    init() {
+        setupDataStoreListener()
+    }
+    
+    private func setupDataStoreListener() {
+        hubListener = Amplify.Hub.publisher(for: .dataStore)
+            .compactMap {
+                $0.eventName == HubPayload.EventName.DataStore.ready ? true : nil
+            }
+            .assign(to: \.isReady, on: self)
+    }
+    
+    func getReady() async {
+        await withCheckedContinuation { continuation in
+            if isReady {
+                continuation.resume()
+            } else {
+                var cancellable: AnyCancellable? = nil
+                cancellable = $isReady.sink { ready in
+                    if ready {
+                        continuation.resume()
+                        cancellable?.cancel()
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension DependencyValues {
+    var dataStore: DataStoreClient {
+        get { self[DataStoreClient.self] }
+        set { self[DataStoreClient.self] = newValue }
+    }
+}
